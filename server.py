dc7dd8dacbbf06084a60b6b82813610186d333e6
Matheus Clemente <matheusclementeg@gmail.com>
2020-03-16 20:45:49 -0300

    fix code host

--- server.py
+++ server.py
@@ -3,5 +3,4 @@ from flask import Flask
 
 app = Flask(__name__)
 app.run(environ.get('PORT'))
-port = int(os.environ.get("PORT", 33507))
-app.run(host='0.0.0.0', port=port)
+app.run(host= '0.0.0.0', port=environ.get('PORT'))
