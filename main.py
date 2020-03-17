b250006e7081fc2e4c81a1a61c4b990a733176f5
Matheus Clemente <matheusclementeg@gmail.com>
2020-03-16 21:29:19 -0300

    update timezone

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import tweepy
 import time
 import time
-from datetime import date
+from datetime import date, datetime, timezone, timedelta
 from os import environ
 
 # Keys for the authentication process
@@ -22,32 +22,29 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 # Infinite loop, tweets once every 24 hours
-while True:
-    try:
-        # Retrievel of API
-        response = requests.get("https://coronavirus-19-api.herokuapp.com/countries")
+try:
+    # Retrievel of API
+    response = requests.get("https://coronavirus-19-api.herokuapp.com/countries")
 
-        # Fetches JSON
-        data = response.json()
-        # Fetches the 'latest' node from the API
-        LatestData = data[29]
+    # Fetches JSON
+    data = response.json()
+    # Fetches the 'latest' node from the API
+    LatestData = data[29]
 
-        # Current Time
-        CurrentTime = date.today()
-        CurrentTimeFormat = '{} / {} / {}'.format(CurrentTime.day, CurrentTime.month, CurrentTime.year)
+    # Current Time
+    CurrentTime = date.today()
+    difference = timedelta(hours=-3)
+    time_zone = timezone(difference)
+    CurrentTimeFormat = '{} / {} / {}'.format(CurrentTime.astimezone(fuso_horario).day, CurrentTime.month, CurrentTime.year)
 
 
-        # Sub nodes from the 'latest' node are fetched along with the current date/time and used in the tweet
-        tweet = "Dados do Coronavírus (COVID-19) - 🇧🇷 \n" +  "\n Data: " + CurrentTimeFormat + "" +  "\n Casos Confirmados: " + str(
-            LatestData['cases']) + "" + "\n Casos Críticos: " + str(
-            LatestData['critical']) + "" + "\n Mortes: " + str(LatestData['deaths']) + "" + "\n Recuperados: " + str(
-            LatestData['recovered']) + "\n" + "\n #COVID19 #Coronavirus"
-        print(tweet)
-        api.update_status(tweet)
-        time.sleep(86400)
-    except tweepy.TweepError as error:
-        print('\nError. Retweet not successful. Reason: ')
-        print(error.reason)
-
-    except StopIteration:
-        break
+    # Sub nodes from the 'latest' node are fetched along with the current date/time and used in the tweet
+    tweet = "Dados do Coronavírus (COVID-19) - 🇧🇷 \n" +  "\n Data: " + CurrentTimeFormat + "" +  "\n Casos Confirmados: " + str(
+        LatestData['cases']) + "" + "\n Casos Críticos: " + str(
+        LatestData['critical']) + "" + "\n Mortes: " + str(LatestData['deaths']) + "" + "\n Recuperados: " + str(
+        LatestData['recovered']) + "\n" + "\n #COVID19 #Coronavirus"
+    print(tweet)
+    api.update_status(tweet)
+except tweepy.TweepError as error:
+    print('\nError. Retweet not successful. Reason: ')
+    print(error.reason)
